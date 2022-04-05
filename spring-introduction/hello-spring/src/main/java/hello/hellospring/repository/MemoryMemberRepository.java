415749763e25e6f518a7c56d57c045cfd8cd93a3
박예찬 <jkyyc3@gmail.com>
2022-04-05 23:34:23 +0900

    CREATE : Client Domain & Repo

---
+++ spring-introduction/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,37 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberInterface {
+
+    // 실무에서는 동시성 문제를 고려해야 하나 여기서는 간단하게만
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        // Optional 을 사용해서 Null 에 대한 처리도 담당한다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
